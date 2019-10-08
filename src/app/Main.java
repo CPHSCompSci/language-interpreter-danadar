bf9755090ce0991625b8f6f8f9d4530315a15ac2
Darren <Darren@10.19.21.172>
2019-10-08 12:37:49 -0500

    decodeMessage Working

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,9 +5,13 @@ public class Main {
   public static void main(String[] args) {
     Lexicon lex = new Lexicon();
 
-    String newWord = lex.translate("");
+    String newWord = lex.translate("thingy");
     System.out.println(newWord);
-
+    System.out.println(decodeMessage(lex.getSample(1)));
+    System.out.println(decodeMessage(lex.getSample(3)));
+    System.out.println(decodeMessage(lex.getSample(5)));
+    System.out.println(decodeMessage(lex.getSample(7)));
+    System.out.println(decodeMessage(lex.getSample(15)));
   }
 
   /**
@@ -16,27 +20,66 @@ public class Main {
    * @param word The single word to be decoded.
    */
   public static String decode(String word) {
-	  int index = word.indexOf("ent");
-	  
-	  if(index + 3 != word.length())
+	  //removes the "ingy" added in language
+	  String newWord = word;
+	  if(word.length() >= 4 && (word.substring(word.length() - 4, word.length())).equals("ingy"))
 	  {
-		  
+		  newWord = word.substring(0, word.length() - 4);
 	  }
-	  else
+	  else if(word.length() >= 5 && (word.substring(word.length() - 5, word.length() - 1)).equals("ingy"))
 	  {
+		  newWord = word.substring(0, word.length() - 5) + word.substring(word.length() - 1);
+	  }
+	  //removes the "ent" added in language
+	  int index = newWord.indexOf("ent");
+	  if(index != -1)
+	  {
+		  char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 		  
+		  for(char v : vowels)
+		  {
+			  if(newWord.charAt(index - 1) == v)
+			  {
+				  if(index + 3 < newWord.length())
+				  {
+					  return newWord.substring(0, index) + newWord.substring(index + 3);
+				  }
+				  else
+				  {
+					  return newWord.substring(0, index);
+				  }
+			  }
+		  }
 	  }
 	  
-	  return "";
+	  return newWord;
   }
-
   /**
    * This method should not have code copy/pasted from the decode method.
    * 
    * @param message The sentence (multiple words) to be decoded.
    */
   public static String decodeMessage(String message) {
-    return "";
+	  String newMessage = "";
+	  String currentWord;
+	  String tempMessage = message;
+	  
+	  for(int currentIndex = 0; currentIndex <= message.length() - 1;)
+	  {
+		  if(tempMessage.indexOf(" ") != -1)
+			  currentWord = tempMessage.substring(0, tempMessage.indexOf(" "));
+		  else
+			  currentWord = tempMessage.substring(0);
+		  
+		  newMessage += decode(currentWord) + " ";
+		  currentIndex = newMessage.length();
+		  
+		  if(tempMessage.indexOf(" ") != -1)
+			  tempMessage = tempMessage.substring(tempMessage.indexOf(" ") + 1);
+		  else
+			  return newMessage;
+	  }
+	  return newMessage;
   }
 
   /**
